f2ec76f93a3f95d24660b32459f56e1c34e42f8e
Andrea Zanchi <andrea.zanchi@gmail.com>
2026-07-23 16:34:42 +0100

    refactoring

---
+++ Samples/Shared/Data/GameProfileOptions.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace Shared.Data
+{
+    public struct GameProfileOptions
+    {
+        public string GameName { get; set; }
+        public string GamePath { get; set; }
+        public bool InUse { get; set; }
+        public int TDP { get; set; }
+        public bool CPUBoost { get; set; }
+        public int CPUEPP { get; set; }
+        public int CPUClock { get; set; }
+        public int FPSLimit { get; set; }
+        public int FPSLimitMode { get; set; }
+        public bool JudderFreeFPS { get; set; }
+        public string ProfilePath { get; set; }
+        public IDictionary<GameId, GameProfile> Cache { get; set; }
+
+        public static GameProfileOptions CreateDefault()
+        {
+            return new GameProfileOptions
+            {
+                GameName = string.Empty,
+                GamePath = string.Empty,
+                InUse = true,
+                TDP = 25,
+                CPUBoost = true,
+                CPUEPP = 80,
+                CPUClock = 0,
+                FPSLimit = 0,
+                FPSLimitMode = 0,
+                JudderFreeFPS = true,
+                ProfilePath = string.Empty,
+                Cache = null
+            };
+        }
+    }
+}
